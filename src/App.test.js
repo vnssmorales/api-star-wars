0c3f161e748fa6d04c633656ecad3e350db4fb49
vanemh <vnss.morales@gmail.com>
2023-05-17 09:32:21 -0400

    test api sw

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Star Wars APP', () => {
+  it('Should show a list of characters including Luke Skywalker', () => {
+    render(<App />);
+    expect(screen.getByText('Luke Skywalker')).toBeInTheDocument();
+  })
+})
