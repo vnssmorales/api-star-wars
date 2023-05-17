4e15ed340421a9a5cccc77c0076d082b0a8e93ad
vanemh <vnss.morales@gmail.com>
2023-05-17 14:47:30 -0400

    visualizacion de personajes y caracteristicas

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import data from './data.json'
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import data from "./data.json";
 
-describe('Star Wars APP', () => {
+describe("Star Wars APP", () => {
   //funcion que hace que se ejecute esto antes de los test
-  beforeAll(() => jest.spyOn(window, 'fetch')); //jest.spyOn para que este vigilando en el objeto window el metodo fetch
+  beforeAll(() => jest.spyOn(window, "fetch")); //jest.spyOn para que este vigilando en el objeto window el metodo fetch
 
-  it('Should show a list of characters from the API', async() => {
-      window.fetch.mockResolvedValueOnce({ //simulación de que el método fetch devuleve lo siguiente
-        ok: true,
-        json: async () => data,
-      });
+  it("Should show a list of characters from the API", async () => {
+    window.fetch.mockResolvedValueOnce({
+      //simulación de que el método fetch devuleve lo siguiente
+      ok: true,
+      json: async () => data,
+    });
 
-      render(<App />);
-      expect(window.fetch).toHaveBeenCalledTimes(1);
-      expect(window.fetch).toHaveBeenCalledWith('http://swapi/dev/api/people/');
+    render(<App />);
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHaveBeenCalledWith("http://swapi.dev/api/people/");
 
-      for (let character of data.results) {
-        expect(await screen.findByText(character.name)).toBeInTheDocument();
-      }
+    for (let character of data.results) {
+      expect(await screen.findByText(character.name)).toBeInTheDocument();
+    }
+  });
+ 
+  it('should show an error message when has a network error', async () => {
+    window.fetch.mockRejectedValueOnce(new Error('Network error'));
+
+    render(<App />);
+    expect(await screen.findByText('Network error')).toBeInTheDocument();
   })
 
 });
--- src/api/people.js
+++ src/api/people.js
@@ -1,5 +1,30 @@
-export async function getPeople() {
-    const response = await fetch('http://swapi/dev/api/people/');
+export async function getPeople(page = 1) {
+  try {
+    const response = await fetch(`http://swapi.dev/api/people/?page=${page}`);
+    if (!response.ok) {
+      throw new NetworkError();
+    }
     const data = await response.json();
     return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function getCharacter(id = 1) {
+  const response = await fetch(`http://swapi.dev/api/people/${id}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function searchCharacter(name) {
+  const response = await fetch(`http://swapi.dev/api/people/?search=${name}`);
+  const data = await response.json();
+  return data;
+}
+
+class NetworkError extends Error {
+  constructor() {
+    super("Network error");
+  }
 }
